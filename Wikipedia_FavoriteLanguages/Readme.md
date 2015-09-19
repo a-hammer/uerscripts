a8641004428ac385c6894688bc4c1f0a248a3a04
Arthur Hammer <arthur@ahammer.me>
2015-09-19 12:37:18 +0200

    Add 'Wikipedia - Favorite Languages' userscript

---
+++ Wikipedia_FavoriteLanguages/Readme.md
@@ -0,0 +1,7 @@
+# Wikipedia - Favorite Languages
+
+Easily access Wikipedia pages in your favorite languages.
+
+**[Install here](https://github.com/a-hammer/userscripts/raw/master/Wikipedia_FavoriteLanguages/wikipedia_favorite-languages.user.js)**.
+
+![](screenshot.jpg)
---
+++ Wikipedia_FavoriteLanguages/wikipedia_favorite-languages.user.js
@@ -0,0 +1,45 @@
+// ==UserScript==
+// @name         Wikipedia - Favorite Languages
+// @description  Easily access Wikipedia pages in your favorite languages.
+//               Add languages below.
+// @version      0.0.1
+// @author       Arthur Hammer
+// @namespace    https://github.com/a-hammer
+// @license      MIT
+// @homepage     https://github.com/a-hammer/userscripts/master/Wikipedia_FavoriteLanguages
+// @updateURL    https://github.com/a-hammer/userscripts/raw/master/Wikipedia_FavoriteLanguages/wikipedia_favorite-languages.user.js
+// @downloadURL  https://github.com/a-hammer/userscripts/raw/master/Wikipedia_FavoriteLanguages/wikipedia_favorite-languages.user.js
+// @match        *.wikipedia.org/wiki/*
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
+// @grant        none
+// ==/UserScript==
+
+(function($) {
+
+    // Add language codes for favorite languages here.
+    // Example: Use 'en' for the English Wikipedia https://en.wikipedia.org
+    // For a list of Wikipedia language see: https://en.wikipedia.org/wiki/List_of_Wikipedias
+    var favoriteLanguages = ['en', 'de'];
+
+    $(function() {
+        var list = $('<ul></ul>')
+            .insertBefore('#firstHeading')
+            .css({
+                'list-style': 'none',
+                'font-size': '12px',
+                'margin-left': '0px',
+                'margin-bottom': '12px'
+            });
+
+        favoriteLanguages.forEach(function(lang) {
+            $('.interlanguage-link.interwiki-' + lang)
+                .clone()
+                .appendTo(list)
+                .css({
+                    'display': 'inline',
+                    'margin-right': '8px'
+                });
+        });
+    });
+
+})(jQuery.noConflict(true));
