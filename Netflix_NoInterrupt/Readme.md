6ea8f6d5e5390e688b862f8ba6e7c12a5090d012
Arthur Hammer <arthur@ahammer.me>
2015-09-22 15:46:56 +0200

    Add 'Netflix - No Interrupt' userscript

---
+++ Netflix_NoInterrupt/Readme.md
@@ -0,0 +1,7 @@
+# Netflix - No Interrupt
+
+Clicks the "continue watching" button whenever it appears.
+
+**[Install here](https://github.com/a-hammer/userscripts/raw/master/Netflix_NoInterrupt/netflix_no-interrupt.user.js)**.
+
+![](screenshot.jpg)
---
+++ Netflix_NoInterrupt/netflix_no-interrupt.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         Netflix - Don't Interrupt
+// @description  Clicks the "continue watching" button whenever it appears.
+// @version      0.0.1
+// @author       Arthur Hammer
+// @namespace    https://github.com/a-hammer
+// @license      MIT
+// @homepage     https://github.com/a-hammer/userscripts/tree/master/Netflix_NoInterrupt
+// @updateURL    https://github.com/a-hammer/userscripts/raw/master/Netflix_NoInterrupt/netflix_no-interrupt.user.js
+// @downloadURL  https://github.com/a-hammer/userscripts/raw/master/Netflix_NoInterrupt/netflix_no-interrupt.user.js
+// @match        http*://www.netflix.com/*
+// @run-at       document-body
+// @grant        none
+// ==/UserScript==
+
+// TODO: Currently every click is followed by an exception in Netflix's JS (netflix.cadmium).
+
+(function() {
+
+    function noInterrupt() {
+        var continuePlaying = document.querySelectorAll('.player-autoplay-interrupter .icon-player-play')[0];
+        if (continuePlaying) continuePlaying.click();
+    }
+
+    new MutationObserver(noInterrupt)
+        .observe(document.body, { childList: true, subtree: true });
+
+})();
