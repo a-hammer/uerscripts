ea8e9af1ff80610400a17c43f76a3a7389030706
Arthur Hammer <arthur@ahammer.me>
2015-09-22 19:15:28 +0200

    Remove jQuery dependency

--- Wikipedia_FavoriteLanguages/wikipedia_favorite-languages.user.js
+++ Wikipedia_FavoriteLanguages/wikipedia_favorite-languages.user.js
@@ -8,37 +8,36 @@
 // @homepage     https://github.com/a-hammer/userscripts/master/Wikipedia_FavoriteLanguages
 // @updateURL    https://github.com/a-hammer/userscripts/raw/master/Wikipedia_FavoriteLanguages/wikipedia_favorite-languages.user.js
 // @downloadURL  https://github.com/a-hammer/userscripts/raw/master/Wikipedia_FavoriteLanguages/wikipedia_favorite-languages.user.js
-// @match        *.wikipedia.org/wiki/*
-// @require      https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
+// @match        http*://*.wikipedia.org/wiki/*
 // @grant        none
 // ==/UserScript==
 
-(function($) {
+(function() {
 
     // Add language codes for favorite languages here.
     // Example: Use 'en' for the English Wikipedia https://en.wikipedia.org
     // For a list of Wikipedia languages see: https://en.wikipedia.org/wiki/List_of_Wikipedias
-    var favoriteLanguages = ['en', 'de'];
+    var languages = ['en', 'de'];
 
-    $(function() {
-        var list = $('<ul></ul>')
-            .insertBefore('#firstHeading')
-            .css({
-                'list-style': 'none',
-                'font-size': '12px',
-                'margin-left': '0px',
-                'margin-bottom': '12px'
-            });
+    var list = document.createElement('ul');
 
-        favoriteLanguages.forEach(function(lang) {
-            $('.interlanguage-link.interwiki-' + lang)
-                .clone()
-                .appendTo(list)
-                .css({
-                    'display': 'inline',
-                    'margin-right': '8px'
-                });
-        });
-    });
+    list.style.listStyle = 'none';
+    list.style.fontSize = '12px';
+    list.style.marginLeft = '0px';
+    list.style.marginBottom = '12px';
 
-})(jQuery.noConflict(true));
+    for (var i = 0; i < languages.length; i++) {
+        var lang = document.querySelectorAll('.interlanguage-link.interwiki-' + languages[i])[0];
+
+        if (lang) {
+            lang = lang.cloneNode(true);
+            lang.style.display = 'inline';
+            lang.style.marginRight = '8px';
+            list.appendChild(lang);
+        }
+    }
+
+    var heading = document.getElementById('firstHeading');
+    if (heading && list.childElementCount > 0) heading.parentElement.insertBefore(list, heading);
+
+})();
