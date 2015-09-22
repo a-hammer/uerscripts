78eb257c335bec9f415312b71a26732e250b337d
Arthur Hammer <arthur@ahammer.me>
2015-09-22 15:52:08 +0200

    Add 'Netflix - Spoiler Spoiler' userscript

---
+++ Netflix_SpoilerSpoiler/Readme.md
@@ -0,0 +1,8 @@
+# Netflix - Spoiler Spoiler
+
+Hides the background spoiler images of upcoming episodes. Works when loading an episode and when switching between episodes.
+
+**[Install here](https://github.com/a-hammer/userscripts/raw/master/Netflix_SpoilerSpoiler/netflix_spoiler-spoiler.user.js)**.
+
+![](screenshot.jpg)
+
---
+++ Netflix_SpoilerSpoiler/netflix_spoiler-spoiler.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         Netflix - Spoiler Spoiler
+// @description  Hides the background spoiler images of upcoming episodes. Works when loading an episode and when switching between episodes.
+// @version      0.0.1
+// @author       Arthur Hammer
+// @namespace    https://github.com/a-hammer
+// @license      MIT
+// @homepage     https://github.com/a-hammer/userscripts/tree/master/Netflix_SpoilerSpoiler
+// @updateURL    https://github.com/a-hammer/userscripts/raw/master/Netflix_SpoilerSpoiler/netflix_spoiler-spoiler.user.js
+// @downloadURL  https://github.com/a-hammer/userscripts/raw/master/Netflix_SpoilerSpoiler/netflix_spoiler-spoiler.user.js
+// @match        http*://www.netflix.com/*
+// @run-at       document-body
+// @grant        none
+// ==/UserScript==
+
+(function() {
+
+     var styles = [
+        '.player-loading-background { display: none; }',           // Episode loading
+        '.player-postplay-background-image { display: none; }',    // Episode transition
+        '.player-postplay { background-color: #222 !important; }'  // More contrast for mini-player
+    ];
+
+    styles.forEach(function(style) {
+        document.styleSheets[0].insertRule(style, 0);
+    });
+
+})();
