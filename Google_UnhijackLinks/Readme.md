99f43e320afdbbe4807b767588cfe2ae0186e68b
Arthur Hammer <arthur@ahammer.me>
2015-09-22 22:55:29 +0200

    Add 'Google - Unhijack Links'

---
+++ Google_UnhijackLinks/Readme.md
@@ -0,0 +1,13 @@
+# Google - Unhijack Links
+
+Disable Google replacing direct search result links with internal Google links. Works on Google Search, Images, and more.
+
+**[Install here.](https://github.com/a-hammer/userscripts/raw/master/Google_UnhijackLinks/google_unhijack-links.user.js)**
+
+Before:
+
+![](screenshot-before.jpg)
+
+After:
+
+![](screenshot-after.jpg)
---
+++ Google_UnhijackLinks/google_unhijack-links.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         Google - Unhijack Links
+// @description  Disable Google replacing direct search result links with internal Google links. Works on Google Search, Images, and more.
+// @version      0.0.1
+// @author       Arthur Hammer
+// @namespace    https://github.com/a-hammer
+// @license      MIT
+// @homepage     https://github.com/a-hammer/userscripts/tree/master/Google_UnhijackLinks
+// @updateURL    https://github.com/a-hammer/userscripts/raw/master/Google_UnhijackLinks/google_unhijack-links.user.js
+// @downloadURL  https://github.com/a-hammer/userscripts/raw/master/Google_UnhijackLinks/google_unhijack-links.user.js
+// @match        https://*.google.tld/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+
+    // Google's 'rwt' function hijacks direct search result links.
+    // Override and disable further overriding.
+    Object.defineProperty(window, 'rwt', {
+        value: function() {},
+        configurable: false,
+        writable: false
+    });
+
+})();
