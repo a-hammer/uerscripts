129315bfbb3d7033360115b437e4ac151a514565
Arthur Hammer <arthur@ahammer.me>
2015-09-22 15:52:38 +0200

    Add 'Netflix - Who's Watching' userscript

---
+++ Netflix_WhosWatching/Readme.md
@@ -0,0 +1,11 @@
+# Netflix - Who's Watching
+
+Skips the profile-chooser page by automatically clicking a specific profile.
+
+Useful if you share a Netflix account but only ever use a single profile on a specific device.
+
+Choose the profile to use in the script.
+
+**[Install here](https://github.com/a-hammer/userscripts/raw/master/Netflix_WhosWatching/netflix_whos-watching.user.js)**.
+
+![](screenshot.jpg)
---
+++ Netflix_WhosWatching/netflix_whos-watching.user.js
@@ -0,0 +1,41 @@
+// ==UserScript==
+// @name         Netflix - Who's Watching
+// @description  Skips the profile-chooser page by automatically clicking a specific profile. Set up the profile to use below.
+// @version      0.0.1
+// @author       Arthur Hammer
+// @namespace    https://github.com/a-hammer
+// @license      MIT
+// @homepage     https://github.com/a-hammer/userscripts/tree/master/Netflix_WhosWatching
+// @updateURL    https://github.com/a-hammer/userscripts/raw/master/Netflix_WhosWatching/netflix_whos-watching.user.js
+// @downloadURL  https://github.com/a-hammer/userscripts/raw/master/Netflix_WhosWatching/netflix_whos-watching.user.js
+// @match        http*://www.netflix.com/*
+// @run-at       document-body
+// @grant        none
+// ==/UserScript==
+
+(function() {
+
+    var profile = 'Arthur';  // Replace with the exact profile name
+
+    function choose() {
+        var chooseProfile = document.querySelectorAll('.choose-profile > .profile > .profile-link');
+
+        for (var i = 0; i < chooseProfile.length; i++) {
+            if (chooseProfile[i].textContent === profile) {
+                chooseProfile[i].click();
+                return;
+            }
+        }
+    }
+
+    new MutationObserver(choose)
+        .observe(document.body, { subtree: true, childList: true });
+
+    choose();
+
+})();
+
+// Profile-chooser page:
+//   .list-profiles > .choose-profile > .profile > .profile-link  > .profile-icon > .profile-name
+// But don't click elements in the profile menu on the main page:
+//   .profile-link > .profile-icon > .profile-name
