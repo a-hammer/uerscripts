93d7200c6888b7053e0cd327fcd7c49daac42510
Arthur Hammer <arthur@ahammer.me>
2015-09-23 14:23:11 +0200

    Add 'Reddit - Highlight User'

---
+++ Reddit_HighlightUser/Readme.md
@@ -0,0 +1,7 @@
+# Reddit - Highlight User
+
+When hovering over a user, highlights that user's comments in the thread. Makes following who said what easier.
+
+**[Install here.](https://github.com/a-hammer/userscripts/raw/master/Reddit_HighlightUser/reddit-highlight-user.user.js)**
+
+![](screenshot.jpg)
---
+++ Reddit_HighlightUser/reddit_highlight-user.user.js
@@ -0,0 +1,40 @@
+// ==UserScript==
+// @name         Reddit - Highlight User
+// @description  When hovering over a user, highlights that user's comments in the thread.
+// @version      0.0.1
+// @author       Arthur Hammer
+// @namespace    https://github.com/a-hammer
+// @license      MIT
+// @homepage     https://github.com/a-hammer/userscripts/tree/master/Reddit_HighlightUser
+// @updateURL    https://github.com/a-hammer/userscripts/raw/master/Reddit_HighlightUser/reddit-highlight-user.user.js
+// @downloadURL  https://github.com/a-hammer/userscripts/raw/master/Reddit_HighlightUser/reddit-highlight-user.user.js
+// @match        https://www.reddit.com/r/*/comments/*
+// @grant        none
+// ==/UserScript==
+
+
+(function() {
+
+    function getUsers(user) {
+        return document.querySelectorAll(user ?
+            '.author[href="https://www.reddit.com/user/' + user + '"]' :
+            '.author'
+        );
+    }
+
+    function highlight(yes) {
+        var users = getUsers(this.textContent);
+        for (var i = 0; i < users.length; i++) {
+            users[i].style.background = yes ? 'yellow'  : 'inherit';
+            users[i].style.padding    = yes ? '2px 5px' : 'inherit';
+        }
+    }
+
+    var users = getUsers();
+
+    for (var i = 0; i < users.length; i++) {
+        users[i].addEventListener('mouseenter', highlight.bind(users[i], true));
+        users[i].addEventListener('mouseleave', highlight.bind(users[i], false));
+    }
+
+})();
