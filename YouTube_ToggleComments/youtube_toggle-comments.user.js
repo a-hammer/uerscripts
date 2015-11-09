7b6d405a0facf10c48192fc14cda4febb9205822
Arthur Hammer <arthur@ahammer.me>
2015-11-09 21:13:40 +0100

    Exclude iframes and cleanup

--- YouTube_ToggleComments/youtube_toggle-comments.user.js
+++ YouTube_ToggleComments/youtube_toggle-comments.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         YouTube - Toggle Comments
 // @description  Adds a button to hide/show comments on YouTube videos. Hides comments by default.
-// @version      0.0.1
+// @version      0.0.2
 // @author       Arthur Hammer
 // @namespace    https://github.com/arthurhammer
 // @license      MIT
@@ -12,6 +12,7 @@
 // @match        https://www.youtube.com/*
 // @run-at       document-end
 // @grant        none
+// @noframes
 // ==/UserScript==
 
 (function() {
@@ -21,48 +22,44 @@
     };
 
     function toggle(el) {
-        if (el) {
-            el.style.display = (el.style.display == 'none') ? 'block' : 'none';
-        }
+        if (el) el.style.display = (el.style.display === 'none') ? 'block' : 'none';
     }
 
-    new MutationObserver(function(_, observer) {
-        var comments = document.getElementsByClassName('comments')[0];
-        if (comments) {
-            observer.disconnect();
-            addToggleComments(comments);
-        }
-    })
-    .observe(document.body, { childList: true, subtree: true });
-
-
-    function addToggleComments(comments) {
+    function addToggleButton(comments) {
+        if (window.self !== window.top) return;
         var commentsHeader = document.getElementsByClassName('all-comments')[0];
-        if (! commentsHeader) return;
+        if (!commentsHeader) return;
 
-        var toggleComments = document.createElement('a');
-
-        toggleComments.textContent = 'Hide';
-        toggleComments.href = '#';
-        toggleComments.style.textTransform = 'none';
-        toggleComments.addEventListener('click', function(e) {
+        var button = document.createElement('a');
+        button.textContent = 'Hide';
+        button.href = '#';
+        button.style.textTransform = 'none';
+        button.addEventListener('click', function(e) {
             e.preventDefault();
+            button.textContent = (button.textContent === 'Hide') ? 'Show' : 'Hide';
 
-            toggleComments.textContent = (toggleComments.textContent === 'Hide') ? 'Show' : 'Hide';
-
-            // Hide/show all the comments stuff
+            // Toggle all the comment-related stuff
             toggle(comments);
             toggle(document.getElementsByClassName('comments-order-menu')[0]);
             toggle(document.getElementById('yt-comments-sb-container'));
         });
 
-        var dash = document.createElement('span');
-        dash.textContent = (' – ');
-        commentsHeader.appendChild(dash);
-        commentsHeader.appendChild(toggleComments);
+        var separator = document.createElement('span');
+        separator.textContent = (' â€” ');
+        commentsHeader.appendChild(separator);
+        commentsHeader.appendChild(button);
 
-        if (prefs.hiddenByDefault) toggleComments.click();
+        if (prefs.hiddenByDefault) button.click();
     }
 
-})();
+    var observer = new MutationObserver(function() {
+        var comments = document.getElementsByClassName('comments')[0];
+        if (comments) {
+            observer.disconnect();
+            addToggleButton(comments);
+        }
+    });
 
+    observer.observe(document.body, { childList: true, subtree: true });
+
+})();
