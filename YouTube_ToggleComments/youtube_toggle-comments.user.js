dd4dfa3d29806b6caf5f3485682dbc7c2fbf44ff
Arthur Hammer <arthur@ahammer.me>
2016-05-04 12:44:36 +0200

    Fix for YouTube layout changes, closes #5

--- YouTube_ToggleComments/youtube_toggle-comments.user.js
+++ YouTube_ToggleComments/youtube_toggle-comments.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         YouTube - Toggle Comments
 // @description  Adds a button to hide/show comments on YouTube videos. Hides comments by default.
-// @version      0.0.2
+// @version      0.0.3
 // @author       Arthur Hammer
 // @namespace    https://github.com/arthurhammer
 // @license      MIT
@@ -9,7 +9,7 @@
 // @updateURL    https://github.com/arthurhammer/userscripts/raw/master/YouTube_ToggleComments/youtube_toggle-comments.user.js
 // @downloadURL  https://github.com/arthurhammer/userscripts/raw/master/YouTube_ToggleComments/youtube_toggle-comments.user.js
 // @supportURL   https://github.com/arthurhammer/userscripts/issues
-// @match        https://www.youtube.com/*
+// @match        https://www.youtube.com/watch?v=*
 // @run-at       document-end
 // @grant        none
 // @noframes
@@ -17,49 +17,65 @@
 
 (function() {
 
-    var prefs = {
-        hiddenByDefault: true
-    };
+  var config = {
+    hidden: true,
+    css:
+      '#comments-toggle-btn {      \
+        color: #767676;            \
+        cursor: pointer;           \
+        font-size: 11px;           \
+        font-weight: 500;          \
+        text-transform: uppercase; \
+      }                            \
+      #comments-toggle-btn:hover { \
+        color: #222;               \
+      }',
+    buttonId: 'comments-toggle-btn',
+    commentsHeaderClass: 'comment-section-header-renderer',
+    commentsSelector: '#comment-section-renderer > *:not(.comment-section-header-renderer)'
+  };
 
-    function toggle(el) {
-        if (el) el.style.display = (el.style.display === 'none') ? 'block' : 'none';
-    }
+  function toggle(el, hide) {
+    if (!el) return;
+    el.style.display = hide ? 'none' : '';
+  }
 
-    function addToggleButton(comments) {
-        if (window.self !== window.top) return;
-        var commentsHeader = document.getElementsByClassName('all-comments')[0];
-        if (!commentsHeader) return;
+  function addCSS(css) {
+    var style = document.createElement('style');
+    style.type = "text/css";
+    style.appendChild(document.createTextNode(css));
+    document.head.appendChild(style);
+  }
 
-        var button = document.createElement('a');
-        button.textContent = 'Hide';
-        button.href = '#';
-        button.style.textTransform = 'none';
-        button.addEventListener('click', function(e) {
-            e.preventDefault();
-            button.textContent = (button.textContent === 'Hide') ? 'Show' : 'Hide';
+  function addToggleButton(target) {
+    if (window.self !== window.top) return;
 
-            // Toggle all the comment-related stuff
-            toggle(comments);
-            toggle(document.getElementsByClassName('comments-order-menu')[0]);
-            toggle(document.getElementById('yt-comments-sb-container'));
-        });
+    var button = document.createElement('button');
+    button.id = config.buttonId;
+    button.textContent = 'Show Less';
+    button.addEventListener('click', function(e) {
+      e.preventDefault();
 
-        var separator = document.createElement('span');
-        separator.textContent = (' — ');
-        commentsHeader.appendChild(separator);
-        commentsHeader.appendChild(button);
-
-        if (prefs.hiddenByDefault) button.click();
-    }
-
-    var observer = new MutationObserver(function() {
-        var comments = document.getElementsByClassName('comments')[0];
-        if (comments) {
-            observer.disconnect();
-            addToggleButton(comments);
-        }
+      var hide = button.textContent === 'Show Less';
+      button.textContent = hide ? 'Show More' : 'Show Less';
+      var comments = document.querySelectorAll(config.commentsSelector);
+      for (var i = 0; i < comments.length; i++) toggle(comments[i], hide);
     });
 
-    observer.observe(document.body, { childList: true, subtree: true });
+    target.appendChild(button);
+    if (config.hidden) button.click();
+  }
+
+  var observer = new MutationObserver(function() {
+    var commentsHeader = document.getElementsByClassName(config.commentsHeaderClass)[0];
+
+    if (commentsHeader) {
+      observer.disconnect();
+      addCSS(config.css);
+      addToggleButton(commentsHeader);
+    }
+  });
+
+  observer.observe(document.body, { childList: true, subtree: true });
 
 })();
