4bf3a53c3933ff2bdbecdf79103063e152acd80d
Arthur Hammer <arthur@ahammer.me>
2016-05-10 11:40:37 +0200

    Layout tweaks, cleanup

--- YouTube_ToggleComments/youtube_toggle-comments.user.js
+++ YouTube_ToggleComments/youtube_toggle-comments.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         YouTube - Toggle Comments
-// @description  Adds a button to hide/show comments on YouTube videos. Hides comments by default.
-// @version      0.0.3
+// @description  Adds a button to hide/show comments on YouTube videos. Hidden by default.
+// @version      0.0.4
 // @author       Arthur Hammer
 // @namespace    https://github.com/arthurhammer
 // @license      MIT
@@ -20,52 +20,24 @@
   var config = {
     hidden: true,
     css:
-      '#comments-toggle-btn {      \
-        color: #767676;            \
-        cursor: pointer;           \
-        font-size: 11px;           \
-        font-weight: 500;          \
-        text-transform: uppercase; \
-      }                            \
-      #comments-toggle-btn:hover { \
-        color: #222;               \
+      '#comments-toggle-btn {                            \
+        color: #767676;                                  \
+        cursor: pointer;                                 \
+        font-size: 11px;                                 \
+        font-weight: 500;                                \
+        text-transform: uppercase;                       \
+      }                                                  \
+      #comments-toggle-btn:hover {                       \
+        color: #222;                                     \
+      }                                                  \
+      .comment-section-header-renderer.comments-hidden { \
+        padding: 0px;                                    \
       }',
     buttonId: 'comments-toggle-btn',
+    commentsHiddenClass: 'comments-hidden',
     commentsHeaderClass: 'comment-section-header-renderer',
     commentsSelector: '#comment-section-renderer > *:not(.comment-section-header-renderer)'
   };
-
-  function toggle(el, hide) {
-    if (!el) return;
-    el.style.display = hide ? 'none' : '';
-  }
-
-  function addCSS(css) {
-    var style = document.createElement('style');
-    style.type = "text/css";
-    style.appendChild(document.createTextNode(css));
-    document.head.appendChild(style);
-  }
-
-  function addToggleButton(target) {
-    var button = document.getElementById(config.buttonId);
-    if (button) return;
-
-    button = document.createElement('button');
-    button.id = config.buttonId;
-    button.textContent = 'Show Less';
-    button.addEventListener('click', function(e) {
-      e.preventDefault();
-
-      var hide = button.textContent === 'Show Less';
-      button.textContent = hide ? 'Show More' : 'Show Less';
-      var comments = document.querySelectorAll(config.commentsSelector);
-      for (var i = 0; i < comments.length; i++) toggle(comments[i], hide);
-    });
-
-    target.appendChild(button);
-    if (config.hidden) button.click();
-  }
 
   var observer = new MutationObserver(function() {
     var commentsHeader = document.getElementsByClassName(config.commentsHeaderClass)[0];
@@ -76,5 +48,40 @@
 
   observer.observe(document.body, { childList: true, subtree: true });
   addCSS(config.css);
+
+  function addToggleButton(target) {
+    var button = document.getElementById(config.buttonId);
+    if (button) return;
+
+    button = document.createElement('button');
+    button.id = config.buttonId;
+    button.textContent = 'Show Less';
+
+    button.addEventListener('click', function(e) {
+      var hide = button.textContent === 'Show Less';
+      button.textContent = hide ? 'Show More' : 'Show Less';
+      var comments = document.querySelectorAll(config.commentsSelector);
+      for (var i = 0; i < comments.length; i++) {
+        toggle(comments[i], hide);
+      }
+      target.classList.toggle(config.commentsHiddenClass);
+    });
+
+    target.appendChild(button);
+    if (config.hidden) button.click();
+  }
+
+  function toggle(el, hide) {
+    if (el) {
+      el.style.display = hide ? 'none' : '';
+    }
+  }
+
+  function addCSS(css) {
+    var style = document.createElement('style');
+    style.textContent = css;
+    document.head.appendChild(style);
+    return style.sheet;
+  }
 
 })();
