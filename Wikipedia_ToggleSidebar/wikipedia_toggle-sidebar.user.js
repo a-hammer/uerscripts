a633f5af4082e9df1f34ee48074aee793110658b
Arthur Hammer <arthur@ahammer.me>
2016-05-11 19:04:01 +0200

    Persist sidebar state, animate sidebar, cleanup
    - Sidebar state is now persisted across pages, closes #7
    - Sidebar is now animated, sliding in
    - Switched from 4 to 2 spaces
    - Reduced layout jumping during load
    - Rewrite and big cleanups

--- Wikipedia_ToggleSidebar/wikipedia_toggle-sidebar.user.js
+++ Wikipedia_ToggleSidebar/wikipedia_toggle-sidebar.user.js
@@ -1,96 +1,153 @@
 // ==UserScript==
 // @name         Wikipedia - Toggle Sidebar
-// @description  Adds a small button to hide or show the sidebar. Makes reading easier.
-// @version      0.2.2
+// @description  Adds a button to hide or show the sidebar. Makes reading easier. Sidebar is hidden by default.
+// @version      0.2.3
 // @author       Arthur Hammer
 // @namespace    https://github.com/arthurhammer
 // @license      MIT
 // @homepage     https://github.com/arthurhammer/userscripts/tree/master/Wikipedia_ToggleSidebar
 // @updateURL    https://github.com/arthurhammer/userscripts/raw/master/Wikipedia_ToggleSidebar/wikipedia_toggle-sidebar.user.js
 // @downloadURL  https://github.com/arthurhammer/userscripts/raw/master/Wikipedia_ToggleSidebar/wikipedia_toggle-sidebar.user.js
+// @supportURL   https://github.com/arthurhammer/userscripts/issues
 // @match        *.wikipedia.org/wiki/*
-// @run-at       document-body
+// @run-at       document-start
 // @grant        none
+// @noframes
 // ==/UserScript==
+
+// Note: Sidebar state is saved per subdomain, i.e. individually for each international Wiki subdomain.
 
 (function() {
 
-    var prefs = {
-        hiddenByDefault: true
-    };
+  var prefs = {
+    hidden: true // Whether to hide sidebar by default
+  };
 
-    var css = '\
-        body { \
-            background-color: white; \
-        } \
-        #content { \
-            border-color: #ddd; \
-            border-left-width: 0; \
-            margin-left: 5em;    \
-            margin-right: 5em; \
-        } \
-        #content.with-sidebar { \
-            margin-left: 10em; \
-            margin-right: 0em; \
-        } \
-        #mw-panel { \
-            display: none; \
-        } \
-        #sidebar-toggle { \
-            position: absolute; \
-            top: 0; \
-            left: 5; \
-            z-index: 999; \
-            padding: 10px; \
-            font-size: 20px; \
-            opacity: 0.5; \
-            cursor: pointer; \
-            transition: all 0.1s; \
-        } \
-        #sidebar-toggle:hover { \
-            font-size: 23px; \
-            opacity: 1; \
-            top: -2px; \
-        }';
+  var sidebarId = 'mw-panel';
+  var mainContentId = 'content';
+  var toggleId = 'btn-sidebar';
+  var sidebarOnClass = 'sidebar-on';
+  var storageKey = 'sidebarHidden';
 
-    function rotate(element, degrees) {
-        var rotation = 'rotate(' + degrees + 'deg)';
-        element.style.WebkitTransform = rotation;
-        element.style.transform = rotation;
-    }
+  // Insert CSS as soon as possible to minimize layout jumping
+  // (i.e. during document-start instead of setting dynamically after elements load)
+  var css =
+    'body {                                \
+       background-color: white !important; \
+     }                                     \
+     #content {                            \
+       border-color: #ddd;                 \
+       border-left-width: 0;               \
+       margin-left: 5em;                   \
+       margin-right: 5em;                  \
+     }                                     \
+     #content.sidebar-on {                 \
+       margin-left: 10em;                  \
+       margin-right: 0em;                  \
+     }                                     \
+     #mw-panel {                           \
+       left: -200px !important;            \
+       z-index: 998;                       \
+       background-color: white;            \
+       transition: .2s ease;               \
+     }                                     \
+     #mw-panel.sidebar-on {                \
+       left: 0 !important;                 \
+       transition: .2s ease;               \
+     }                                     \
+     #btn-sidebar {                        \
+       position: absolute;                 \
+       top: 0;                             \
+       left: 5;                            \
+       z-index: 999;                       \
+       margin: 10px;                       \
+       font-size: 22px;                    \
+       opacity: 0.5;                       \
+       cursor: pointer;                    \
+       transition: all 0.1s;               \
+     }                                     \
+     #btn-sidebar:hover {                  \
+       opacity: 1;                         \
+     }                                     \
+     #btn-sidebar.sidebar-on {             \
+       -transform: rotate(90deg);          \
+       -webkit-transform: rotate(90deg);   \
+     }';
 
-    function addCSS(css) {
-        var style = document.createElement('style');
-        style.textContent = css;
-        document.head.appendChild(style);
-    }
+  document.addEventListener('DOMContentLoaded', init);
+  init();   // Listener might never be called, double-check
 
-    function addSidebarToggle() {
-        if (!document.body || document.getElementById('sidebar-toggle')) return;
-
-        var sidebarToggle = document.createElement('div');
-        sidebarToggle.id = 'sidebar-toggle';
-        sidebarToggle.title = 'Toggle Sidebar';
-        sidebarToggle.textContent = '▸';
-
-        sidebarToggle.addEventListener('click', function() {
-            var sidebar = document.getElementById('mw-panel');
-            var mainContent = document.getElementById('content');
-
-            rotate(sidebarToggle, mainContent.classList.contains('with-sidebar') ? 0 : 90);
-            mainContent.classList.toggle('with-sidebar');
-            sidebar.style.display =
-                (!sidebar.style.display || sidebar.style.display === 'none') ? 'block' : 'none';
-        });
-
-        if (! prefs.hiddenByDefault) sidebarToggle.click();
-
-        document.body.insertBefore(sidebarToggle, document.body.firstElementChild);
-    }
-
-    document.addEventListener('DOMContentLoaded', addSidebarToggle);
+  function init() {
     addCSS(css);
-    // DOMContentLoaded can fire before the listener was installed. Double-check.
-    addSidebarToggle();
+    addToggle();
+    loadSidebar();
+    hideSidebar(prefs.hidden);
+  }
+
+  function addToggle() {
+    if (!document.body || document.getElementById(toggleId)) return;
+
+    var toggle = document.createElement('div');
+    toggle.id = toggleId;
+    toggle.title = 'Toggle Sidebar';
+    toggle.textContent = '▸';
+
+    toggle.addEventListener('click', function() {
+      if (hideSidebar(!prefs.hidden)) {
+        saveSidebar(!prefs.hidden);
+      }
+    });
+
+    document.body.insertBefore(toggle, document.body.firstChild);
+  }
+
+  function hideSidebar(hide){
+    var sidebar = document.getElementById(sidebarId);
+    var main = document.getElementById(mainContentId);
+    var toggle = document.getElementById(toggleId);
+    if (!sidebar || !main || !toggle) return false;
+
+    sidebar.classList.toggle(sidebarOnClass, !hide);
+    main.classList.toggle(sidebarOnClass, !hide);
+    toggle.classList.toggle(sidebarOnClass, !hide);
+    return true;
+  }
+
+  function loadSidebar() {
+    if (storageAvailable()) {
+      var data = localStorage.getItem(storageKey);
+      prefs.hidden =
+        (data === 'true')  ? true :
+        (data === 'false') ? false : prefs.hidden;
+    }
+  }
+
+  function saveSidebar(hidden) {
+    prefs.hidden = hidden;
+    if (storageAvailable()) {
+      localStorage.setItem(storageKey, hidden);
+    }
+  }
+
+  // from: https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API
+  function storageAvailable(type) {
+    try {
+      var storage = window[type || 'localStorage'];
+      var x = '__storage_test__';
+      storage.setItem(x, x);
+      storage.removeItem(x);
+      return true;
+    }
+    catch(e) {
+      return false;
+    }
+  }
+
+  function addCSS(css) {
+    var style = document.createElement('style');
+    style.textContent = css;
+    document.head.appendChild(style);
+    return style.sheet;
+  }
 
 })();
